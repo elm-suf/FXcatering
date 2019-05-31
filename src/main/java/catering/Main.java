3deda548872182849786639a2f23aa9a33905024
elm-suf <elm.sufian@gmail.com>
2019-05-31 12:03:55 +0200

    Initial commit

---
+++ src/main/java/catering/Main.java
@@ -0,0 +1,42 @@
+package catering;
+
+import catering.businesslogic.CateringAppManager;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private CateringAppManager app;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        this.primaryStage = primaryStage;
+        this.app = CateringAppManager.getInstance();
+
+        FXMLLoader mainLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent main = mainLoader.load();
+        Scene mainScene = new Scene(main);
+
+        MainController mainController = mainLoader.getController();
+
+        primaryStage.setScene(mainScene);
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(600);
+        // primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
+
+    public void openMenuEditing() {
+
+    }
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module catnring {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+
+    opens catering to javafx.fxml;
+    exports catering;
+}
