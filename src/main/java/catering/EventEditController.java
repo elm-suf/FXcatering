24f711d28a2ac6858094610ac74a315ee11a6b35
elm-suf <elm.sufian@gmail.com>
2019-06-20 08:57:44 +0200

    table cell factory + showing some detail info

--- src/main/java/catering/EventEditController.java
+++ src/main/java/catering/EventEditController.java
@@ -3,14 +3,13 @@ package catering;
 import catering.businesslogic.grasp_controllers.Task;
 import catering.businesslogic.managers.CateringAppManager;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -48,20 +47,61 @@ public class EventEditController {
     @FXML
     private AnchorPane detail_task;
 
+
+    //    Detail Task FXML
+    @FXML
+    private TextField recipe_txf;
+
+    @FXML
+    private ComboBox<?> shift_combo;
+
+    @FXML
+    private ComboBox<?> cook_combo;
+
+    @FXML
+    private TextField quantity_txf;
+
+    @FXML
+    private ComboBox<?> duration_combo;
+
+    @FXML
+    private TextField difficulty_txf;
+
+    @FXML
+    private Button position_down_btn;
+
+    @FXML
+    private Button position_up_btn;
+
+    private
+
+
     @FXML
     void initialize() {
         initTaskList();
         event_edit_back_btn.setOnMouseClicked(e -> goBack());
-        task_recipe.setCellValueFactory(new PropertyValueFactory<>("recipe"));
         task_is_assigned.setCellValueFactory(tc -> new SimpleBooleanProperty(tc.getValue().isAssigned()));
         task_is_completed.setCellValueFactory(tc -> new SimpleBooleanProperty(tc.getValue().isCompleted()));
-        task_index.setCellValueFactory(new PropertyValueFactory<>("index"));
+        task_index.setCellValueFactory(tc -> new SimpleIntegerProperty(tc.getValue().getIndex()).asObject());
+        task_recipe.setCellValueFactory(tc -> new SimpleObjectProperty<>(tc.getValue().getRecipe().toString()));
 
+        task_list.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         task_list.getSelectionModel().selectedIndexProperty().addListener((observable) -> {
             selectedTask = task_list.getSelectionModel().getSelectedItem();
+            //todo
+            showDetailedView(selectedTask);
         });
+    }
 
+    private void showDetailedView(Task selectedTask) {
+        detail_task.setVisible(true);
+        System.out.println("Selectd : " + selectedTask);
+        recipe_txf.setText(selectedTask.getRecipe().toString());
 
+        //todo load cook in cook_combo
+        //todo load shifts in shift_combo
+        //todo setItems duration_combo
+        //todo wire the save button
     }
 
     private void initTaskList() {
