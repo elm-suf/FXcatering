c105b9c62f2af38d444cbbac4f1d9b3ee387363d
elm-suf <elm.sufian@gmail.com>
2019-06-22 18:47:15 +0200

    jfxListView select event works

--- src/main/java/catering/AssignTasksController.java
+++ src/main/java/catering/AssignTasksController.java
@@ -4,14 +4,15 @@ import catering.businesslogic.exceptions.AssignTaskException;
 import catering.businesslogic.grasp_controllers.CatEvent;
 import catering.businesslogic.grasp_controllers.User;
 import catering.businesslogic.managers.CateringAppManager;
+import com.jfoenix.controls.JFXListView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.AnchorPane;
 
@@ -24,16 +25,16 @@ import java.util.ResourceBundle;
 public class AssignTasksController implements Initializable {
 
     @FXML
+    JFXListView<CatEvent> jfx_list;
+
+    @FXML
     private Label title_label;
 
     @FXML
     private AnchorPane root_pane;
 
-    @FXML
-    private Button assign_btn;
 
-    @FXML
-    private ListView<CatEvent> events_listview;
+    ObservableList<CatEvent> catEvents;
 
     private AnchorPane editEvent;
     private EventEditController eventEditController;
@@ -44,23 +45,35 @@ public class AssignTasksController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        initData();
+        initView();
+    }
 
-
+    private void initData() {
         this.currentUser = CateringAppManager.userManager.getCurrentUser();
+
+        events = CateringAppManager.eventManager.getAllEvents(currentUser);
+        catEvents = FXCollections.observableList(events);
+    }
+
+    private void initView() {
         title_label.setText("Bentornato " + currentUser.toString());
-        //todo fare chiamata a datamanger per ottenere la lista di eventi
 
-        events_listview.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        this.initList();
-
-        events_listview.getSelectionModel().selectedIndexProperty().addListener((observable) -> {
-            selectedEvent = events_listview.getSelectionModel().getSelectedItem();
-            assign_btn.setDisable(selectedEvent == null);
+        jfx_list.setItems(catEvents);
+        jfx_list.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        jfx_list.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue ov, Number value, Number newVal) {
+                if (newVal.intValue() >= 0)
+                    openEditEvent(events.get(newVal.intValue()));
+            }
         });
-        assign_btn.setOnMouseClicked((ev) -> {
-            System.out.println(selectedEvent);
+    }
+
+    private void openEditEvent(CatEvent prop) {
+        if (jfx_list.getSelectionModel().getSelectedIndex() >= 0) {
             try {
-                CateringAppManager.eventManager.selectEvent(selectedEvent);
+                CateringAppManager.eventManager.selectEvent(prop);
             } catch (AssignTaskException e) {
                 e.printStackTrace();
             }
@@ -72,12 +85,6 @@ public class AssignTasksController implements Initializable {
                 e.printStackTrace();
             }
             root_pane.getChildren().setAll(editEvent);
-        });
-    }
-
-    private void initList() {
-        events = CateringAppManager.eventManager.getAllEvents(currentUser);
-        ObservableList<CatEvent> catEvents = FXCollections.observableList(events);
-        events_listview.setItems(catEvents);
+        }
     }
 }
